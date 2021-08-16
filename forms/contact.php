047c8d5b1208b107aefe5315c313a7f5aa566df9
Aishwarya <aishtupe@gmail.com>
2021-08-16 16:15:39 +0530

    3rd message

--- forms/contact.php
+++ forms/contact.php
@@ -11,29 +11,33 @@ if (!empty($name) || !empty($email) || !empty($subject) || !empty($message))
   $dbPassword="";
   $dbname="enquiry";
   $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-  if (mysqli_connect_error()){
+  if (mysqli_connect_error())
+  {
     die('Connect Error('.mysqli_connect_error().')'.mysqli_connect_error());
-  }else{
+  }
+  else
+  {
     $SELECT = "SELECT email From enquiry Where email = ? Limit 1";
     $INSERT = "INSERT Into enquiry(name, email, subject, message) values(?,?,?,?)";
     $stmt=$conn->prepare($SELECT);
     $stmt->bind_result($email);
     $stmt->store_result();
     $rnum = $stmt->num_rows;
-    if($rnum==0){
+    if($rnum==0)
+    {
       $stmt->close();
 
       $stmt=$conn->prepare($INSERT);
       $stmt->bind_param("ssss",$name,$email,$subject,$message);
       $stmt->execute();
       echo "Message Sent Successfully";
-    }else{
+    }
+    else
+    {
       echo "Someone already register using this email";
     }
     $stmt->close();
-    $conn->close();
-    }
-    }
+    $conn->close(); 
   }
 }
 else{
